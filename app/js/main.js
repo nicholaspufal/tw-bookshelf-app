78ace65238966b69951b24859ae4665627b9ffde
Nicholas Pufal <npufal@thoughtworks.com>
2014-01-08 11:04:55 -0200

    Cleaning up and using an angular directive to wrap the call to bootstrap's popover

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-  $("li.book").popover({ placement: "auto top", trigger: "hover", html: true })
-});
+angular.module('twBookshelf',[]).
+  directive('twPopover', function() {
+    return {
+      link: function (scope, element, attributes) {
+        var options = {
+          placement: 'auto top',
+          trigger: 'hover',
+          html: true
+        }
+
+        $(element).popover(options);
+      }
+    };
+  });
